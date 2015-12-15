bc669a95e8b6f7149f7bb973129c1f391f7f96ea
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2015-12-15 22:51:58 +0100

    Added specific registration of WASD, as well as logging of other keys. Fixed refresh not working

--- js/input.js
+++ js/input.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
+	console.log("input.js loaded");
 	$(document).keydown(function(event){
 		var keycode = event.which;
-		if (keycode === keycode == 122 || keycode == 123) {
-			console.log("F11 or F12 pressed")
+		if (keycode == 122 || keycode == 123 || keycode === 116) {
+			console.log("F11, F12, or F5 pressed");
 			return;
+		} else if (keycode === 87) { //W key
+			console.log("W was pressed");
+			onLoadd();
+		} else if (keycode === 65) { //A key
+			console.log("A was pressed");
+		}  else if (keycode === 83) { //S key
+			console.log("S was pressed");
+		}  else if (keycode === 68) { //D key
+			console.log("D was pressed");
+		} else {
+			console.log("Keycode " + keycode + " pressed");
 		}
+
 		event.preventDefault();
-	}
+	});
 })
