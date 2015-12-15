b74c483c603e5a7e4ace0a39e2950151054b83b0
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2015-12-15 22:54:41 +0100

    Setup for clearer separation of game and rendering logic

---
+++ js/game.js
@@ -0,0 +1,13 @@
+/** Called when page is loaded*/
+function start() {
+	console.log("game.start");
+
+}
+
+/** Called every new frame
+ * @param {number} deltatime The time since the last rendered frame
+ */
+function update(deltatime) {
+	console.log("game.update, deltatime=" + deltatime + " ms");
+
+}
--- js/init.js
+++ js/init.js
@@ -2,8 +2,39 @@ var canvasElement;
 var canvas;
 
 $(document).ready(function(){
+	console.log("init.js loaded");
 	canvasElement = document.getElementById("canvas");
 	canvas = canvasElement.getContext("2d");
-	canvas.height = canvasElement.height;
-	canvas.width = canvasElement.width;
-})
+
+	// Update canvas size upon resize
+	$(window).resize(function(){
+		canvasElement.width = window.innerWidth;
+        canvasElement.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        console.log("Canvas width: " + canvas.width);
+        console.log("Canvas height: " + canvas.height);
+	});
+
+	_onLoad();
+});
+
+var _timeSinceLastRender;
+function _renderFrame(timestamp) {
+	//console.log("init._renderFrame")
+	var deltatime = timestamp - _timeSinceLastRender;
+	_timeSinceLastRender = timestamp;
+
+	update(deltatime);
+
+	window.requestAnimationFrame(_renderFrame);
+}
+
+function _onLoad(){
+	//console.log("init._onLoad")
+	$(window).resize();
+	start();
+	_timeSinceLastRender = performance.now();
+	window.requestAnimationFrame(_renderFrame);
+}
