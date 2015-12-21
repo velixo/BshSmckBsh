8f5b9a34ca1d729b8a14f51eb44bb4cb63e88d26
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2015-12-21 17:10:09 +0100

    Added asking for playername

--- js/game.js
+++ js/game.js
@@ -10,17 +10,13 @@ var platform3;
 
 var player;
 var animManager;
+var playerName;
 
 /** Called when page is loaded*/
 function start() {
 	console.log("game.start");
+	playerName = prompt("Enter your name:");
 	world = new World();
-//	floor = new Floor(30);
-//	lwall = new Wall('l', 30);
-//	rwall = new Wall('r', 30);
-//	platform1 = new Rectangle(250, 600, 300, 40, "rectbroad");
-//	platform2 = new Rectangle(700, 150, 100, 600, "recttall");
-//	platform3 = new Rectangle(350, 800, 400, 40, "rectbroadunder");
 	loadLevel1();
 	animManager = new AnimationManager();
 	console.log("animManager = " + animManager.toString());
@@ -33,16 +29,8 @@ function start() {
 function update(deltatime) {
 	world.update();
 	player.update(deltatime);
-
-//	floor.draw();
-//	lwall.draw();
-//	rwall.draw();
-//	platform1.draw();
-//	platform2.draw();
-//	platform3.draw();
 	world.draw();
 	drawCircle(player.x + player.width/2, player.y + player.width/2, player.width/2);
-	drawText(player.x, player.y - 35, "Pale ale", "#0d0");
-
+	drawText(player.x, player.y - 35, playerName, "#0d0");
 	animManager.drawAnimations();
 }
